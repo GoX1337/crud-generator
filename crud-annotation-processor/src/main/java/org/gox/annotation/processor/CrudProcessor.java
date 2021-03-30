9ba01a80e93fcb95d998ca48c5346f831f4693e2
GoX1337 <gox.roche@gmail.com>
2021-03-30 18:56:12 +0200

    Generate first useless class at compile time

--- crud-annotation-processor/src/main/java/org/gox/annotation/processor/CrudProcessor.java
+++ crud-annotation-processor/src/main/java/org/gox/annotation/processor/CrudProcessor.java
@@ -6,7 +6,12 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 import java.util.Set;
 
 @SupportedAnnotationTypes("javax.persistence.Entity")
@@ -15,15 +20,37 @@ public class CrudProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnv) {
-        System.out.println("CrudProcessor processing...");
+        System.out.println("CrudProcessor processing..." + new Date());
 
         for (TypeElement annotation : set) {
             Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation);
-
-            annotatedElements.stream().forEach(e -> System.out.println(e));
+            annotatedElements.forEach(this::printClassElement);
         }
 
         System.out.println("CrudProcessor process ends.");
         return true;
+    }
+
+    public void printClassElement(Element element) {
+        if(ElementKind.CLASS.equals(element.getKind())) {
+            System.out.println(element);
+            element.getEnclosedElements().forEach(this::printAttributeElement);
+        }
+
+        JavaFileObject builderFile = null;
+        try {
+            builderFile = processingEnv.getFiler().createSourceFile("Hey");
+            try (PrintWriter out = new PrintWriter(builderFile.openWriter())) {
+                out.println("public class Hey { }");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printAttributeElement(Element element){
+        if(ElementKind.FIELD.equals(element.getKind())) {
+            System.out.println("\t- " + element);
+        }
     }
 }
--- crud-app/src/main/java/org/gox/crud/app/entity/User.java
+++ crud-app/src/main/java/org/gox/crud/app/entity/User.java
@@ -1,14 +1,26 @@
 package org.gox.crud.app.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue
     private UUID id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int age;
 }
